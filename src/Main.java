26c4cec98439609d573828a43df48f38ca979808
FloraSuccess <f.success2898@gmail.com>
2025-04-11 20:47:59 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+         System.out.println("I love pasta!");
+        System.out.println("It should be with cream-cheese sous");
+        System.out.println();
+        }
+}
